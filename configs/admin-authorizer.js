396712d3075f518dc5892e397b0fa9b67b8d8a7b
warner110 <185422149+warner110@users.noreply.github.com>
2025-07-28 21:05:26 -0400

    change

--- configs/admin-authorizer.js
+++ configs/admin-authorizer.js
@@ -1,13 +1,13 @@
 import admin from "firebase-admin";
 
-import serviceAccount from "../second-shot-firebase-adminsdk.json" assert { type: "json" };
+// import serviceAccount from "../second-shot-firebase-adminsdk.json" assert { type: "json" };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://second-shot.firebaseio.com",
-    storageBucket: "gs://second-shot-1d28a.firebasestorage.app",
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://second-shot.firebaseio.com",
+//     storageBucket: "gs://second-shot-1d28a.firebasestorage.app",
+//   });
+// }
 
 export { admin };
